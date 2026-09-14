3ad06f937f30f8115650f4a175c3094bdc8200ee
Vincy Zhang <vincyz@hotmail.com>
2026-09-14 17:05:54 -0400

    Update Category.java

---
+++ Category.java
@@ -0,0 +1,11 @@
+/**
+ * Represents the allowed categories for expenses in PocketPlan.
+ */
+public enum Category {
+    FOOD,
+    TRANSPORTATION,
+    ENTERTAINMENT,
+    HOUSING,
+    UTILITIES,
+    OTHER
+}
