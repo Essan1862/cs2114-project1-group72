a626fa71d0bcf6633d800bb8a1e38f031482e5b9
mckennab25 <mbryan2029@gmail.com>
2026-09-16 17:57:08 -0400

    Making getters and setters for Expense class

--- Expense.java
+++ Expense.java
@@ -20,5 +20,41 @@ public class Expense
         this.description = description;
         this.date = date;
     }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public double getAmount() {
+        return amount;
+    }
+    
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    
+    public Category getCategory() {
+        return category;
+    }
+    
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+    
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 
 }
