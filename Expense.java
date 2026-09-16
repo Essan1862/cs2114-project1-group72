33514d3552093cd9b61eae08d071862d570cab06
mckennab25 <mbryan2029@gmail.com>
2026-09-16 17:41:43 -0400

    Starting to initialize and declare expense variables

--- Expense.java
+++ Expense.java
@@ -1,1 +1,24 @@
+public class Expense
+{
 
+    private int id;
+    private double amount;
+    private Category category;
+    private String desrciption;
+    private LocalDate date;
+
+    public Expense(
+        int id,
+        double amount,
+        Category category,
+        String description,
+        LocalDate date)
+    {
+        this.id = id;
+        this.amount = amount;
+        this.category = category;
+        this.description = description;
+        this.date = date;
+    }
+
+}
