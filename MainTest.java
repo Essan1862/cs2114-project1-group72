6a196870cf47a126ae7cdc7ea07045daf1461dba
Essan1862 <salemessan@gmail.com>
2026-09-18 16:13:39 -0400

    Create MainTest.java again because of wrong name

---
+++ MainTest.java
@@ -0,0 +1,29 @@
+import student.TestCase;
+
+public class MainTest extends TestCase {
+
+    public void testMainExitImmediately() {
+        systemIn("8\n");
+
+        Main.main(new String[] {});
+
+        String output = systemOut().getHistory();
+
+        assertTrue(output.contains("PocketPlan"));
+        assertTrue(output.contains("Goodbye."));
+    }
+
+    public void testMainEmptyInputDoesNotCrash() {
+        systemIn("");
+        boolean crashed = false;
+
+        try {
+            Main.main(new String[] {});
+        }
+        catch (Exception e) {
+            crashed = true;
+        }
+
+        assertFalse(crashed);
+    }
+}
